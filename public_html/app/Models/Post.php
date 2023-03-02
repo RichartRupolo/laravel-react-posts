0f463cacc45ad82e1ae557eb35770aeea761b724
Richart Rupolo <richart.rupolo@hotmail.com>
2023-03-02 13:47:16 -0300

    Update Post.php
    set table name

--- public_html/app/Models/Post.php
+++ public_html/app/Models/Post.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
-
+    protected $table = 'posts';
     /**
      * The attributes that are mass assignable.
      *
